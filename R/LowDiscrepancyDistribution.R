001a0fde1d9c14bd6034fa745c59b47ae4b14040
SigurdJanson <emailjs@web.de>
2020-03-29 20:57:16 +0200

    Added low-discrepancy functions to distribute starting points in parameter space

---
+++ R/LowDiscrepancyDistribution.R
@@ -0,0 +1,97 @@
+
+#' .HarmonyR
+#' @describeIn .Harmony
+.HarmonyR <- function( Dim, MaxDepth = 50L, .Depth = 0 ) {
+  HarmonyConstants <- c(
+    1 + sqrt(5/4)-0.5,   # Fibonacci 
+    1.32471795724474602596090885447809, # Plastic number
+    1.22074408460575947536168534910883  # 
+  )
+  if (Dim < length(HarmonyConstants))
+    return(HarmonyConstants[Dim])
+    
+  if(.Depth == MaxDepth) 
+    return(1)
+  else {
+    result <- .HarmonyR(Dim, MaxDepth = MaxDepth, .Depth = .Depth+1)
+    return( (1 + result )^(1/(Dim+1)) )
+  }
+}
+
+
+#' .Harmony
+#' Compute harmonious numbers for a number of dimensions `Dim`.
+#' @param Dim Number of dimensions of the space.
+#' @param Tolerance Desired precision. The sequence stops if the 
+#' following summand is smaller than tolerance. The default 
+#' value is close to 1.5e-8 (numeric ≥ 0).
+#' @param MaxDepth Number of recursive
+#' @param .Depth Used by recursive algormithm
+#' @note `.HarmonyR` function uses a recursive algorithm.
+#' @source Roberts, M. (2018). The Unreasonable Effectiveness 
+#' of Quasirandom Sequences. https://t1p.de/z30y
+#' @author Jan Seifert
+#' @return
+#' @examples
+.Harmony <- function( Dim, Tolerance = sqrt(.Machine$double.eps) ) {
+  HarmonyConstants <- c(
+    1 + sqrt(5/4)-0.5,   # Fibonacci 
+    1.32471795724474602596090885447809, # Plastic number 
+    1.22074408460575947536168534910883  # 
+  )
+  if (Dim < length(HarmonyConstants))
+    return(HarmonyConstants[Dim])
+
+  x <- 1.0 
+  repeat {
+    new <- (1 + x)^(1/(Dim+1))
+    if(abs(new - x) < Tolerance) return(new)
+    x <- new
+  }
+}
+
+
+
+#' GetHarmonicPoints
+#' Computes a `Dim`-dimensional low discrepancy 
+#' quasirandom sequences using 'Harmonious numbers'.
+#' @param Dim The number of dimensions.
+#' @param N The number of samples to compute.
+#' @param Seed Any number between 0 and 1 (default 0.5).
+#' @details This function is uses quasi-random properties of 
+#' infinite sequences. At the dimension of 1 it is equal to the
+#' Finbonacci number. Because of it's properties it is recommended
+#' to use this function with larger values of `N` (probably >= 50 
+#' for 2 dimensions).
+#' 
+#' Seed can be any number. A common default setting 
+#' is `Seed = 0`. but Roberts (2018) recommends `Seed = 0.5`. Change
+#' `Seed` to get varying results.
+#' @return A matrix with `Dim` columns and `N` rows. Values range
+#' from zero to one.
+#' @export
+#'
+#' @references Roberts, M. (2018). The Unreasonable Effectiveness 
+#' of Quasirandom Sequences. https://t1p.de/z30y
+#' @examples
+GetHarmonicPoints <- function( Dim, N = 20, Seed = 0.5 ) {
+  # d=2   # dimensions  # n=50  # number of required points 
+  G <- .Harmony(Dim) # g = phi(d)
+  
+  # for j in range(d): 
+  #   alpha[j] = pow(1/g,j+1) %1 
+  Alpha <- (1/G^(1:Dim)) %% 1
+  
+  Z <- matrix(nrow = N, ncol = Dim)
+  #for i in range(n): 
+  #  z[i] = (seed + Alpha*(i+1)) %1 
+  for (d in 1:Dim) {
+    Z[, d] <- (Seed + Alpha[d] * 1:N) %% 1
+  }
+  #print(Z)
+  return(Z)
+}
+
+# x <- GetHarmonicPoints(5, 100)
+# x <- GetHarmonicPoints(2, 50)
+# plot(x[,1], x[,2])
---
+++ test/test_LowDiscrepancyDistribution.R
@@ -0,0 +1,71 @@
+library(testthat)
+source("../R/LowDiscrepancyDistribution.R")
+
+
+test_that("Harmony: Compare Linear and Recursive Function", {
+  for(d in c(1:20, 30, 40, 50 , 100)) {
+    expect_equal(.HarmonyR(d), .Harmony(d))
+  }
+})
+
+
+test_that("Harmony Linear", {
+  # Fibonacci constant
+  phi <- 1.61803398874989484820458683436563
+  expect_equal(.Harmony(1), phi)
+  phi <- 1 + sqrt(5/4)-0.5 
+  expect_equal(.Harmony(1), phi)
+  
+  # Plastic number (https://oeis.org/A072117)
+  phi <- 1.32471795724474602596090885447809
+  expect_equal(.Harmony(2), phi)
+  
+  phi <- 1.22074408460575947536168534910883
+  expect_equal(.Harmony(3), phi)
+  
+  phi <- 1.1673 # provided by Marohnić & Strmečki
+  expect_equal(.Harmony(4), phi, tolerance = 1E-3)
+  
+  phi <- 1.1347
+  expect_equal(.Harmony(5), phi, tolerance = 1E-3)
+  
+  phi <- 1.1128
+  expect_equal(.Harmony(6), phi, tolerance = 1E-3)
+  
+  #phi <- 1.0970 # seems off
+  #expect_equal(.Harmony(6), phi, tolerance = 1E-3)
+})
+
+
+test_that("Harmony Recursive", {
+  # Fibonacci constant
+  phi <- 1.61803398874989484820458683436563
+  expect_equal(.HarmonyR(1), phi) #-0.0643
+  phi <- 1 + sqrt(5/4)-0.5 
+  expect_equal(.Harmony(1), phi)
+
+  # Plastic number  
+  phi <- 1.32471795724474602596090885447809
+  expect_equal(.HarmonyR(2), phi) #-0.0124
+  
+  phi <- 1.22074408460575947536168534910883
+  expect_equal(.HarmonyR(3), phi) #-0.00436
+})
+
+
+test_that("GetHarmonicPoints", {
+  Dim <- 2L
+  N <- 100L
+  o <- GetHarmonicPoints(Dim, N)
+  expect_equal(dim(o), c(N, Dim))
+  expect_true(all(o < 1))
+  expect_true(all(o > 0))
+
+  Dim <- 5L
+  N <- 200L
+  o <- GetHarmonicPoints(Dim, N)
+  expect_equal(dim(o), c(N, Dim))
+  expect_true(all(o < 1))
+  expect_true(all(o > 0))
+})
+
