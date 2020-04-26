8c857eb97a6d3003e22cf419c933a0b3e4dd5434
SigurdJanson <emailjs@web.de>
2020-04-26 20:26:23 +0200

    Added comments to understand the function

--- R/nmkb.R
+++ R/nmkb.R
@@ -2,27 +2,34 @@
 #' Nelder-Mead with Box constraints
 #' @note see https://rdrr.io/cran/dfoptim/man/nmkb.html
 #' @source This function is a copy of dfoptim::nmkb(). This algorithm is based 
-#' on the Matlab code of Prof. C.T. Kelley, given in his book "Iterative methods 
-#' for optimization". It is implemented here with the permission of Prof. Kelley 
-#' and SIAM. However, there are some non-trivial modifications of the algorithm 
-#' made by Ravi Varadhan.
-nmkb <- function (par, fn, lower = -Inf, upper = Inf, control = list(), ...) 
-{
+#' on the [Matlab code of Prof. C.T. Kelley, given in his book "Iterative methods 
+#' for optimization"](https://archive.siam.org/books/kelley/fr18/OPT_CODE/nelder.m). It was implemented for R by Ravi Varadhan with permission of 
+#' Prof. Kelley and SIAM. However, there are some non-trivial modifications of 
+#' the algorithm made by Ravi Varadhan.
+# TODO: Add DOI 10.1007/s10589-010-9329-3
+# TODO: find speed optimisations
+nmkb <- function (par, fn, lower = -Inf, upper = Inf, control = list(), ...) {
   ctrl <- list(tol = 1e-06, maxfeval = min(5000, max(1500, 20 * length(par)^2)), 
                regsimp = TRUE, maximize = FALSE, restarts.max = 3, trace = FALSE)
   namc <- match.arg(names(control), choices = names(ctrl), several.ok = TRUE)
   if (!all(namc %in% names(ctrl))) 
-    stop("unknown names in control: ", namc[!(namc %in% names(ctrl))])
-  if (!is.null(names(control))) 
-    ctrl[namc] <- control
+    stop("unknown names in 'control': ", namc[!(namc %in% names(ctrl))])
+  if (!is.null(names(control))) ctrl[namc] <- control
   ftol <- ctrl$tol
   maxfeval <- ctrl$maxfeval
   regsimp <- ctrl$regsimp
   restarts.max <- ctrl$restarts.max
   maximize <- ctrl$maximize
   trace <- ctrl$trace
-  n <- length(par)
+
+  # Dimensions of paramter space
+  n <- length(par) 
+  if (n == 1) 
+    stop(call. = FALSE, "Use `optimize` for univariate optimization")
+  if (n > 30) 
+    warning("Nelder-Mead should not be used for high-dimensional optimization")
   
+  # Spatial distortion functions to handle box constraints
   g <- function(x) {
     gx <- x
     gx[c1] <- atanh(2 * (x[c1] - lower[c1]) / (upper[c1] - lower[c1]) - 1)
@@ -39,6 +46,7 @@ nmkb <- function (par, fn, lower = -Inf, upper = Inf, control = list(), ...)
     gix
   }
   
+  # Set variables required to handle box constraints
   if (length(lower) == 1) lower <- rep(lower, n)
   if (length(upper) == 1) upper <- rep(upper, n)
   
@@ -48,107 +56,115 @@ nmkb <- function (par, fn, lower = -Inf, upper = Inf, control = list(), ...)
   upp.finite <- is.finite(upper)
   c1 <- low.finite & upp.finite  # both lower and upper bounds are finite 
   c2 <- !(low.finite | upp.finite) # both lower and upper bounds are infinite
-  c3 <- !(c1 | c2) & low.finite # finite lower bound, but infinite upper bound
-  c4 <- !(c1 | c2) & upp.finite  # finite upper bound, but infinite lower bound
+  c3 <- !(c1 | c2) & low.finite # finite lower bound, infinite upper bound
+  c4 <- !(c1 | c2) & upp.finite  # finite upper bound, infinite lower bound
   
-  if (all(c2)) stop("Use `nmk()' for unconstrained optimization!", call.=FALSE)
+  if (all(c2)) stop("Use `nmk()` for unconstrained optimization!", call.=FALSE)
   
-  if (maximize) 
-    fnmb <- function(par) -fn(ginv(par), ...)
-  else fnmb <- function(par) fn(ginv(par), ...)
+  # Set objective function
+  #.if (maximize) 
+  maximize <- ifelse(maximize, -1, 1)
+  fnmb <- function(par, ...) fn(ginv(par), ...) * maximize
+  #.else fnmb <- function(par, ...) fn(ginv(par), ...)
   
-  x0 <- g(par)
-  if (n == 1) 
-    stop(call. = FALSE, "Use `optimize' for univariate optimization")
-  if (n > 30) 
-    warning("Nelder-Mead should not be used for high-dimensional optimization")
+  x0 <- g(par) # x0 is the starting point
+  
+  # Initial values: V - vertices of the simplex. f - result of objective function.
   V <- cbind(rep(0, n), diag(n))
   f <- rep(0, n + 1)
-  f[1] <- fnmb(x0)
+  f[1] <- fnmb(x0, ...)
   V[, 1] <- x0
   scale <- max(1, sqrt(sum(x0^2)))
-  if (regsimp) {
-    alpha <- scale/(n * sqrt(2)) * c(sqrt(n + 1) + n - 1, 
-                                     sqrt(n + 1) - 1)
+
+  if (regsimp) { # Create regular simplex
+    alpha <- scale/(n * sqrt(2)) * c(sqrt(n + 1) + n - 1, sqrt(n + 1) - 1)
     V[, -1] <- (x0 + alpha[2])
     diag(V[, -1]) <- x0[1:n] + alpha[1]
-    for (j in 2:ncol(V)) f[j] <- fnmb(V[, j])
-  }
-  else {
+    for (j in 2:ncol(V)) f[j] <- fnmb(V[, j], ...)
+  } else {
     V[, -1] <- x0 + scale * V[, -1]
-    for (j in 2:ncol(V)) f[j] <- fnmb(V[, j])
+    for (j in 2:ncol(V)) f[j] <- fnmb(V[, j], ...)
   }
   f[is.nan(f)] <- Inf
-  nf <- n + 1
-  ord <- order(f)
+  nf <- n + 1     # number of calls of objective function
+  # Sort simplex vertices according to f(v)
+  ord <- order(f) 
   f <- f[ord]
   V <- V[, ord]
-  rho <- 1
-  gamma <- 0.5
-  chi <- 2
-  sigma <- 0.5
-  conv <- 1
-  oshrink <- 1
-  restarts <- 0
-  orth <- 0
-  dist <- f[n + 1] - f[1]
+  
+  rho <- 1     # reflection  alpha
+  chi <- 2     # expansion   beta
+  gamma <- 0.5 # contraction gamma
+  sigma <- 0.5 # shrinkage   delta
+  
+  # Setting oshrink=0 gives vanilla Nelder-Mead, i.e. the simplex is merely
+  # reflected without expansion, contraction, or shrinking
+  oshrink <- 1L
+  
+  orth <- 0 #TODO: this variable has no explanation
+  
   v <- V[, -1] - V[, 1]
-  delf <- f[-1] - f[1]
-  diam <- sqrt(colSums(v^2))
+  delf <- f[-1] - f[1]          # "delta f"
+  diam <- sqrt(colSums(v^2))    # diameter of the simplex???
   sgrad <- c(solve(t(v), delf))
   alpha <- 1e-04 * max(diam)/sqrt(sum(sgrad^2))
+  
+  # Termination criteria (`nf` has been defined above)
   simplex.size <- sum(abs(V[, -1] - V[, 1]))/max(1, sum(abs(V[, 1])))
-  itc <- 0
-  conv <- 0
-  message <- "Succesful convergence"
-  while (nf < maxfeval & restarts < restarts.max & dist > ftol & 
-         simplex.size > 1e-06) {
-    fbc <- mean(f)
+  dist <- f[n + 1] - f[1] # distance between highest and lowest vertex f()
+  restarts <- 0 # number of restarts
+  
+  # Exit code and message
+  conv <- 99
+  message <- "Unexpected termination"
+
+  itc <- 0 # iteration counter
+  while (nf < maxfeval & restarts < restarts.max & dist > ftol & simplex.size > 1e-06) {
     happy <- 0
-    itc <- itc + 1
-    xbar <- rowMeans(V[, 1:n])
+    itc <- itc + 1 
+    
+    # REFLECT (is always first try)
+    fbc <- mean(f)
+    xbar <- rowMeans(V[, 1:n])  # centroid of the simplex
     xr <- (1 + rho) * xbar - rho * V[, n + 1]
-    fr <- fnmb(xr)
+    fr <- fnmb(xr, ...)
     nf <- nf + 1
-    if (is.nan(fr)) 
-      fr <- Inf
+    if (is.nan(fr)) fr <- Inf
+
     if (fr >= f[1] & fr < f[n]) {
+      # Reflection was successful
       happy <- 1
       xnew <- xr
       fnew <- fr
-    }
-    else if (fr < f[1]) {
+    } else if (fr < f[1]) { # if reflected point f(r) < f(old): EXPAND
       xe <- (1 + rho * chi) * xbar - rho * chi * V[, n + 1]
-      fe <- fnmb(xe)
-      if (is.nan(fe)) 
-        fe <- Inf
+      fe <- fnmb(xe, ...)
+      if (is.nan(fe)) fe <- Inf
       nf <- nf + 1
       if (fe < fr) {
         xnew <- xe
         fnew <- fe
         happy <- 1
-      }
-      else {
+      } else {
         xnew <- xr
         fnew <- fr
         happy <- 1
       }
-    }
-    else if (fr >= f[n] & fr < f[n + 1]) {
+    } else if (fr >= f[n] & fr < f[n + 1]) {
+      # OUTSIDE CONTRACTION
       xc <- (1 + rho * gamma) * xbar - rho * gamma * V[, n + 1]
-      fc <- fnmb(xc)
-      if (is.nan(fc)) 
-        fc <- Inf
+      fc <- fnmb(xc, ...)
+      if (is.nan(fc)) fc <- Inf
       nf <- nf + 1
       if (fc <= fr) {
         xnew <- xc
         fnew <- fc
         happy <- 1
       }
-    }
-    else if (fr >= f[n + 1]) {
+    } else if (fr >= f[n + 1]) {
+      # INSIDE CONTRACTION
       xc <- (1 - gamma) * xbar + gamma * V[, n + 1]
-      fc <- fnmb(xc)
+      fc <- fnmb(xc, ...)
       if (is.nan(fc)) 
         fc <- Inf
       nf <- nf + 1
@@ -158,13 +174,13 @@ nmkb <- function (par, fn, lower = -Inf, upper = Inf, control = list(), ...)
         happy <- 1
       }
     }
-    if (happy == 1 & oshrink == 1) {
+    # Test for sufficient decrease; do oriented shrink if necessary
+    if (happy == 1 & oshrink == 1L) {
       fbt <- mean(c(f[1:n], fnew))
       delfb <- fbt - fbc
       armtst <- alpha * sum(sgrad^2)
       if (delfb > -armtst/n) {
-        if (trace) 
-          cat("Trouble - restarting: \n")
+        if (trace) cat("Trouble - restarting: \n")
         restarts <- restarts + 1
         orth <- 1
         diams <- min(diam)
@@ -174,19 +190,19 @@ nmkb <- function (par, fn, lower = -Inf, upper = Inf, control = list(), ...)
         diag(V[, -1]) <- diag(V[, -1]) - diams * sx[1:n]
       }
     }
+    # New point accepted; remove old point and restart
     if (happy == 1) {
       V[, n + 1] <- xnew
       f[n + 1] <- fnew
       ord <- order(f)
       V <- V[, ord]
       f <- f[ord]
-    }
-    else if (happy == 0 & restarts < restarts.max) {
-      if (orth == 0) 
-        orth <- 1
+    } else if (happy == 0 & restarts < restarts.max) {
+      if (orth == 0) orth <- 1
       V[, -1] <- V[, 1] - sigma * (V[, -1] - V[, 1])
-      for (j in 2:ncol(V)) f[j] <- fnmb(V[, j])
+      for (j in 2:ncol(V)) f[j] <- fnmb(V[, j], ...)
       nf <- nf + n
+      # Sort
       ord <- order(f)
       V <- V[, ord]
       f <- f[ord]
@@ -198,18 +214,18 @@ nmkb <- function (par, fn, lower = -Inf, upper = Inf, control = list(), ...)
     f[is.nan(f)] <- Inf
     dist <- f[n + 1] - f[1]
     sgrad <- c(solve(t(v), delf))
+    
     if (trace & !(itc%%2)) 
       cat("iter: ", itc, "\n", "value: ", f[1], "\n")
-  }
+  } #while
+  
   if (dist <= ftol | simplex.size <= 1e-06) {
     conv <- 0
     message <- "Successful convergence"
-  }
-  else if (nf >= maxfeval) {
+  } else if (nf >= maxfeval) {
     conv <- 1
     message <- "Maximum number of fevals exceeded"
-  }
-  else if (restarts >= restarts.max) {
+  } else if (restarts >= restarts.max) {
     conv <- 2
     message <- "Stagnation in Nelder-Mead"
   }
@@ -220,20 +236,27 @@ nmkb <- function (par, fn, lower = -Inf, upper = Inf, control = list(), ...)
 
 
 
+rosbkext <- function(x) {
+  # Extended Rosenbrock function
+  n <- length(x)
+  sum (100*(x[1:(n-1)]^2 - x[2:n])^2 + (x[1:(n-1)] - 1)^2)
+}
 
-
-
-
-
-# rosbkext <- function(x){
-#   # Extended Rosenbrock function
-#   n <- length(x)
-#   sum (100*(x[1:(n-1)]^2 - x[2:n])^2 + (x[1:(n-1)] - 1)^2)
-# }
+np <- 10
+#set.seed(123)
+p0 <- rnorm(np)
+p0[p0 > +2] <- +2 - 1E-8
+p0[p0 < -2] <- -2 + 1E-8
+e <- nmkb(fn = rosbkext, par = p0, lower = -2, upper = 2)
+# o <- nmkb(fn = rosbkext, par = p0, lower = -2, upper = 2)
 # 
-# np <- 10
-# set.seed(123)
-# 
-# p0 <- rnorm(np)
-# #xm1 <- nmk(fn=rosbkext, par=p0) # maximum `fevals' is not sufficient to find correct minimum
-# xm1b <- nmkb(fn=rosbkext, par=p0, lower=-2, upper=2, control = list(maxfeval = 9e6))
+ ctrl <- list(maxfeval = 50000)
+ #set.seed(123)
+ p0 <- rnorm(np)
+ p0[p0 > +2] <- +2 - 1E-8
+ p0[p0 < -2] <- -2 + 1E-8
+ e <- nmkb(fn = rosbkext, par = p0, lower = -2, upper = 2, control = ctrl)
+# o <- nmkb(fn = rosbkext, par = p0, lower = -2, upper = 2, control = ctrl)
+
+
+
